f6c121cd522b235b5f324d9f915a1d03a9ea42a8
dongsu <netfron@gmail.com>
2017-08-28 01:53:30 +0900

    redis session driver, laravel passport(oauth) 적용

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('profile', require('./components/Profile.vue'));
 
 const app = new Vue({
     el: '#app'
