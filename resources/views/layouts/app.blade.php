1538bade2d1288ff8bdcf9f1d956d78197a467de
dongsu <netfron@gmail.com>
2017-10-10 15:49:04 +0900

    about 추가

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,7 @@
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="http://about.dlog.io" target="_blank">About</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
